958b44d4360c658150a3c348e86341b123a029ef
Thai Nguyen <discoverylight@yahoo.com>
2019-09-09 16:46:52 -0700

    added a front-end app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Accounts from "./components/Accounts";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Accounts />
+    </div>
+  );
+}
+
+export default App;
